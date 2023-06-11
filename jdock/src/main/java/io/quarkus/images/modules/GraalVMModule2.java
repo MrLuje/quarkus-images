fa6f0c19482cef0306ba7d6280b1860ff766797d
MrLuje <ogmniluje@gmail.com>
2023-06-11 21:39:26 +0000

    957MB

--- jdock/src/main/java/io/quarkus/images/modules/GraalVMModule2.java
+++ jdock/src/main/java/io/quarkus/images/modules/GraalVMModule2.java
@@ -6,17 +6,19 @@ import io.quarkus.images.commands.*;
 
 import java.util.List;
 
-public class GraalVMModule extends AbstractModule {
+public class GraalVMModule2 extends AbstractModule {
     public static final String GRAALVM_HOME = "/opt/graalvm";
     private final String url;
     private final String sha;
     private final String filename;
 
     private static final String TEMPLATE = """
-            %s/bin/gu --auto-yes install native-image \\
-              && rm -Rf %s""";
+            tar xzf %s -C /opt \\
+              && mv /opt/graalvm-ce-*-%s* /opt/graalvm
+            """;
+    private final String graalvmVersion;
 
-    public GraalVMModule(String version, String arch, String javaVersion, String sha) {
+    public GraalVMModule2(String version, String arch, String javaVersion, String sha) {
         super("graalvm",
                 arch != null ? version + "-java" + javaVersion + "-" + arch : version + "-java" + javaVersion + "-amd64");
 
@@ -30,18 +32,18 @@ public class GraalVMModule extends AbstractModule {
         this.url = "https://github.com/graalvm/graalvm-ce-builds/releases/download/vm-%s/graalvm-ce-java%s-linux-%s-%s.tar.gz"
                 .formatted(version, javaVersion, arch, version);
         this.sha = sha;
+        this.graalvmVersion = version;
     }
 
     @Override
     public List<Command> commands(BuildContext bc) {
         Artifact artifact = bc.addArtifact(new Artifact(filename, url, sha));
         String script = TEMPLATE.formatted(
-                GRAALVM_HOME, // gu
-                "/tmp/" + artifact.name); // rm
+                "/tmp/" + artifact.name,
+                graalvmVersion); // tar
 
         return List.of(
-                new EnvCommand("JAVA_HOME", GRAALVM_HOME, "GRAALVM_HOME", GRAALVM_HOME),
-                new MicrodnfCommand("fontconfig", "freetype-devel"),
+                new CopyCommand(artifact, "/tmp/" + artifact.name),
                 new RunCommand(script));
     }
 }
--- quarkus-graalvm-builder-image/src/main/java/io/quarkus/images/QuarkusGraalVMBuilder.java
+++ quarkus-graalvm-builder-image/src/main/java/io/quarkus/images/QuarkusGraalVMBuilder.java
@@ -9,30 +9,39 @@ import java.util.Map;
 
 public class QuarkusGraalVMBuilder {
 
-    public static Dockerfile getGraalvmDockerFile(String base, String version, String javaVersion, String arch, String sha) {
-        Dockerfile df = Dockerfile.from(base);
-        df
-                .user("root")
-                .install("tar", "gzip", "gcc", "glibc-devel", "zlib-devel", "shadow-utils", "unzip", "gcc-c++")
-                .install("glibc-langpack-en")
-                .module(new UsLangModule())
-                .module(new QuarkusUserModule())
-                .module(new QuarkusDirectoryModule())
-                .module(new UpxModule(arch))
-                .module(new GraalVMModule(version, arch, javaVersion, sha))
-                .env("PATH", "$PATH:$JAVA_HOME/bin")
-                .label("io.k8s.description", "Quarkus.io executable image providing the `native-image` executable.",
-                        "io.k8s.display-name", "Quarkus.io executable (GraalVM Native)",
-                        "io.openshift.tags", "executable,java,quarkus,graalvm,native",
-                        "maintainer", "Quarkus Team <quarkus-dev@googlegroups.com>")
-                .user("1001")
-                .workdir("/project")
-                .entrypoint("native-image");
+    public static MultiStageDockerFile getGraalvmDockerFile(String base, String version, String javaVersion, String arch,
+            String sha) {
+        MultiStageDockerFile df = Dockerfile.multistages()
+                .stage("get-github-release",
+                        Dockerfile.from(base)
+                                .user("root")
+                                .install("tar", "gzip")
+                                .module(new GraalVMModule2(version, arch, javaVersion, sha)))
+                .stage("final-image",
+                        Dockerfile.from(base)
+                                .user("root")
+                                .install("tar", "gzip", "gcc", "glibc-devel", "zlib-devel", "shadow-utils", "unzip", "gcc-c++")
+                                .install("glibc-langpack-en")
+                                .module(new UsLangModule())
+                                .module(new QuarkusUserModule())
+                                .module(new QuarkusDirectoryModule())
+                                .module(new UpxModule(arch))
+                                .copyFromStage("get-github-release", "/opt", "/opt")
+                                .module(new GraalVMModule(version, arch, javaVersion, sha))
+                                .env("PATH", "$PATH:$JAVA_HOME/bin")
+                                .label("io.k8s.description",
+                                        "Quarkus.io executable image providing the `native-image` executable.",
+                                        "io.k8s.display-name", "Quarkus.io executable (GraalVM Native)",
+                                        "io.openshift.tags", "executable,java,quarkus,graalvm,native",
+                                        "maintainer", "Quarkus Team <quarkus-dev@googlegroups.com>")
+                                .user("1001")
+                                .workdir("/project")
+                                .entrypoint("native-image"));
 
         return df;
     }
 
-    public static Dockerfile getGraalvmDockerFile(Config.ImageConfig image, Variant variant, String base) {
+    public static MultiStageDockerFile getGraalvmDockerFile(Config.ImageConfig image, Variant variant, String base) {
         return getGraalvmDockerFile(base, image.graalvmVersion(), Integer.toString(image.javaVersion()), variant.arch(),
                 variant.sha());
     }
@@ -40,7 +49,7 @@ public class QuarkusGraalVMBuilder {
     public static Map<String, Buildable> collect(Config.ImageConfig image, String base) {
         Map<String, Buildable> architectures = new HashMap<>();
         for (Variant variant : image.variants) {
-            Dockerfile df = QuarkusGraalVMBuilder.getGraalvmDockerFile(image, variant, base);
+            MultiStageDockerFile df = QuarkusGraalVMBuilder.getGraalvmDockerFile(image, variant, base);
             architectures.put(variant.arch(), df);
         }
         return architectures;
